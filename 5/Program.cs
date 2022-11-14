e60f8ab76ec4e1ca639e5bb61977ab7483dc9bc6
Kate <cvetok_spb@bk.ru>
2022-11-14 13:18:16 +0300

    Task5

---
+++ 5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+// Первые два элемента последовательности задаются пользователем
+
+Console.WriteLine("Введите целое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество чисел ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void SummNumbers(int num1, int num2, int N)
+{
+    if (N <= 1) Console.Write($", {num1 + num2}");
+    else
+    {
+        Console.Write($", {num1 + num2}");
+        SummNumbers(num2, num1 + num2, N - 1);
+    }
+}
+
+
+Console.Write($"{num1}, {num2}");
+SummNumbers(num1, num2, N);
+

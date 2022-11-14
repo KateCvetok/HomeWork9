96b44e55e08f3ce9fe5bfd26b43aa3ed61773007
Kate <cvetok_spb@bk.ru>
2022-11-14 12:50:55 +0300

    Task2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,1 +1,18 @@
 ﻿// Найти сумму элементов от M до N, N и M заданы
+
+Console.WriteLine("Введите целое число ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SummNumbers(int M, int N)
+{
+
+    if (M == N) return M;
+
+    else if (M < N) return N + SummNumbers(M, N - 1);
+    else return N + SummNumbers(M, (N + 1));
+}
+
+SummNumbers(M, N);
+Console.WriteLine($"Сумма элементов от {M} до {N} = " + SummNumbers(M, N));
